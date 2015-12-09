8f72b2aa6c14ebc904d2b2a07553c21c4c1dc2b7
Arnob Alam <arnoblalam@gmail.com>
2015-12-09 00:47:55 -0500

    Smaller tree

--- larger_tree.py
+++ larger_tree.py
@@ -3,34 +3,29 @@ import Tree
 artificial_tree = {
 1:[2,3,4],
 2:[5,6],
-3:[7,8,9],
+3:[7,9],
 4:[10,11,12,13],
-5:[14,15,16,17],
+5:[14,16,17],
 6:[18,19],
-7:[20,21,22,23,24],
-8:[],
+7:[20,21,23,24],
 9:[],
-10:[25,26,27],
+10:[26,27],
 11:[],
-12:[28,29,30],
+12:[28,29],
 13:[],
 14:[],
-15:[],
 16:[],
 17:[],
 18:[],
 19:[],
 20:[],
 21:[],
-22:[],
 23:[],
 24:[],
-25:[],
 26:[],
 27:[],
 28:[],
-29:[],
-30:[],
+29:[]
 }
 
 artificial_tree_weights ={
@@ -41,34 +36,30 @@ artificial_tree_weights ={
 5: 10,
 6: 30,
 7: 20,
-8: 40,
 9: 400,
 10:50,
 11:30,
 12:80,
 13:60,
 14:20,
-15:30,
 16:150,
 17:80,
 18:40,
 19:200,
 20:10,
 21:130,
-22:50,
 23:80,
 24:60,
-25:70,
 26:260,
 27:30,
 28:40,
-29:10,
-30:20,
+29:10
 }
 
-reduced_artificial_trees = Tree.agg_to(artificial_tree, 9)
-
-agg_weights = [Tree.apply_aggregation(t, artificial_tree_weights) for t in reduced_artificial_trees]
-H = [Tree.calculate_H(x) for x in agg_weights]
-S = [Tree.calculate_S(x) for x in agg_weights]
-Tree.aggregate(artificial_tree,artificial_tree_weights, 9, keep_intermediate=True, order_matters=False)
+#reduced_artificial_trees = Tree.agg_to(artificial_tree, 28, keep_intermediate=False, order_matters=False)
+Tree.aggregate(artificial_tree, artificial_tree_weights, 23, keep_intermediate=True)
+#
+#agg_weights = [Tree.apply_aggregation(t, artificial_tree_weights) for t in reduced_artificial_trees]
+#H = [Tree.calculate_H(x) for x in agg_weights]
+#S = [Tree.calculate_S(x) for x in agg_weights]
+#Tree.aggregate(artificial_tree,artificial_tree_weights, 29, keep_intermediate=True, order_matters=False)
